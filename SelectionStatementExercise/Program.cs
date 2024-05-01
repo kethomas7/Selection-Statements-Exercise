bb00206ac6dcc735a7adce73f4d28a32d3f02fa0
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-05-01 10:35:25 -0500

    Completed the IF Else Exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            int favNumber = r.Next(1,4);
+
+
+            Console.WriteLine("Can you read minds? Guess what my favorite number is?");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+
+            {
+                Console.WriteLine("Your answer is too low, keep guessing!");
+
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Your answer is too high, keep guessing!");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it! Maybe you can read minds!");
+            }
         }
+
     }
 }
